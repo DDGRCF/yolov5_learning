7a69a4cb4df044768c4cee766c6f7047acf8026e
DDGRCF <raochaofan1970@gmail.com>
2021-08-04 12:52:41 +0800

    -

---
+++ my_experiments/detect_fire_prune.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate yolov5
+
+cd ..
+python detect.py --weights runs/train/exp17/weights/pruning_best.pt --source data/images/fire --use-pruning
+cd -
---
+++ my_experiments/train_fire_prune.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate yolov5
+cd ..
+python train.py --epochs 200 --weights weights/yolov5l.pt --cfg models/yolov5l.yaml \
+--data data/fire.yaml --batch-size 16 --workers 8 --device 0 --use-pruning --layer-rate 0.80 \
+--layer-gap 0,320,3 --skip-downsample
+cd -
